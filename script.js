500186ed4bdeeba0ac89ba02cacb40d87409aef6
KelvynKhrystian <kelvynkhrystian@pop-os.localdomain>
2022-05-28 02:55:30 -0300

    função apagar anterior

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ function limpar() {
     document.querySelector('#resultado').innerHTML = " "
 }
 
+// função para apagar o ultimo numero colocado
+function apagar() {
+    const result = document.querySelector('#resultado').innerHTML;
+    document.querySelector('#resultado').innerHTML = result.substring(0, result.length -1)
+}
